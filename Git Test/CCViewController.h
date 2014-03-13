1c1b6f8254e02c54c91e029fe24254b8a0209c80
David Sanders <sanderdb01@Davids-MacBook-Pro.local>
2014-03-12 22:48:56 -0400

    Initial Commit

---
+++ Git Test/CCViewController.h
@@ -0,0 +1,13 @@
+//
+//  CCViewController.h
+//  Git Test
+//
+//  Created by David Sanders on 3/12/14.
+//  Copyright (c) 2014 Code Coalition. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CCViewController : UIViewController
+
+@end
