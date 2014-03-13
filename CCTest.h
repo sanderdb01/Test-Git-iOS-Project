1eea5ef3f584adf29afdf05b5d14902c0d06297b
David Sanders <sanderdb01@gmail.com>
2014-03-12 23:32:12 -0400

    added test class

---
+++ CCTest.h
@@ -0,0 +1,13 @@
+//
+//  CCTest.h
+//  Git Test
+//
+//  Created by David Sanders on 3/12/14.
+//  Copyright (c) 2014 Code Coalition. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CCTest : NSObject
+
+@end
---
+++ CCTest.m
@@ -0,0 +1,13 @@
+//
+//  CCTest.m
+//  Git Test
+//
+//  Created by David Sanders on 3/12/14.
+//  Copyright (c) 2014 Code Coalition. All rights reserved.
+//
+
+#import "CCTest.h"
+
+@implementation CCTest
+
+@end
